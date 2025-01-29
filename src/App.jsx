96fbeb8bb946542fef4935036a95681eb3c821da
mafidfrontend <neo.uz001@gmail.com>
2025-01-29 18:26:14 +0500

    finished

---
+++ src/App.jsx
@@ -0,0 +1,117 @@
+import "./App.css";
+
+function App() {
+    // 1-topshiriq /////////////////////////////////////////////////
+    // const users = [
+    //     { name: "Ali", age: 17 },
+    //     { name: "Bobur", age: 22 },
+    //     { name: "Sardor", age: 15 },
+    //     { name: "Madina", age: 25 },
+    // ];
+    // const filtered = users.filter((item) => {
+    //     if (item.age > 18) {
+    //         return item;
+    //     }
+    // });
+    // console.log(filtered)
+    // 2-topshiriq ////////////////////////////////////////////////////
+    // const users = [
+    //     { name: "Ali", age: 20 },
+    //     { name: "Bobur", age: 22 },
+    //     { name: "Sardor", age: 25 },
+    // ];
+    // const f = users.map((item)=>{
+    //     const l = item.name
+    //     return l
+    // });
+    // console.log(f)
+    // 3-topshiriq /////////////////////////////////////////////////////
+    // const products = [
+    //     { name: "Telefon", price: 1200000 },
+    //     { name: "Noutbuk", price: 5000000 },
+    //     { name: "Dazmol", price: 400000 },
+    //     { name: "Sovutgich", price: 2500000 },
+    // ];
+    // const newArr = products.filter((item)=>{
+    //     if(item.price > 500000) {
+    //         return item
+    //     }
+    // })
+    // console.log(newArr)
+    // 4-topshiriq /////////////////////////////////////////////////////
+    // const students = [
+    //     { name: "Ali", grade: 4 },
+    //     { name: "Bobur", grade: 3 },
+    //     { name: "Madina", grade: 5 },
+    // ];
+    // const newArr = students.map((item) => {
+    //     return item.grade + 1;
+    // });
+    // console.log(newArr)
+    // 5-topshiriq /////////////////////////////////////////////////////
+    // const names = ["ali", "bobur", "madina", "sardor"];
+    // const newArr = names.map((item)=>{
+    //     return item.toLocaleUpperCase()
+    // })
+    // console.log(newArr)
+    // 6-topshiriq /////////////////////////////////////////////////////
+    // const products = [
+    //     { name: "Telefon", price: 1200000 },
+    //     { name: "Noutbuk", price: 5000000 },
+    //     { name: "Dazmol", price: 400000 },
+    // ];
+    // const newArr = products.map((item) => {
+    //     return item.price - (item.price / 10);
+    // });
+    // console.log(newArr);
+    // 7-topshiriq /////////////////////////////////////////////////////
+    // const users = [
+    //     { name: "Ali", age: 20 },
+    //     { name: "Bobur", age: 22 },
+    //     { name: "Anvar", age: 25 },
+    //     { name: "Madina", age: 18 },
+    // ];
+    // const newArr = users.filter((item) => {
+    //     if (item.name.startsWith('A')) {
+    //         return item;
+    //     }
+    // });
+    // console.log(newArr)
+    // 8-topshiriq /////////////////////////////////////////////////////
+    // const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    // const toq = numbers.filter((item) => item % 2 === 1);
+    // console.log(toq)
+    // 9-topshiriq ///////////////////////////////////////////////////// chala
+    // const workers = [
+    //     { name: "Ali", salary: 2000000 },
+    //     { name: "Madina", salary: 4000000 },
+    //     { name: "Sardor", salary: 2500000 },
+    // ];
+    // workers.map((item) => {
+    //     if (item.salary > 3000000) {
+    //         const secondArr = [{ position: "juda kam" }];
+    //         workers.concat(secondArr);
+    //     }
+    // });
+    // console.log(workers);
+    // 10-topshiriq /////////////////////////////////////////////////////
+    // const products = [
+    //     { name: "Telefon", price: 1200000 },
+    //     { name: "Noutbuk", price: 5000000 },
+    //     { name: "Dazmol", price: 400000 },
+    //     { name: "Sovutgich", price: 2500000 },
+    // ];
+    // const newArr = products.map((item) => {
+    //     if(item.price > 500000) {
+    //         const price = item.price - (item.price * 15 / 100);
+    //         return {name: item.name, price: price}
+    //     }
+    //     else {
+    //         return item
+    //     }
+    // });
+    // console.log(newArr);
+}
+
+export default App;
+
